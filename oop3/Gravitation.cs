ffe96fcd2a51ab3d432b280ec31411c055c27712
amali <amali@PROGRAMARE>
2023-05-04 22:27:34 +0300

    Add project files.

---
+++ oop3/Gravitation.cs
@@ -0,0 +1,46 @@
+﻿//acceleratia gravitationala pe luna = 1,6  m / s^2 (Reprezintă schimbarea vitezei unui obiect în timp);
+// accceleratia gravitationala pe pamant = 9,807;
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop3
+{
+    class Gravitation
+    {
+        private readonly double weight;
+
+        private readonly double gravityOfMoon;
+
+        private readonly double gravityOfEarth;
+
+
+        // constructor 
+
+        public Gravitation(double weight, double gravityOfMoon, double gravityOfEarth)
+        {
+            this.weight = weight;
+
+            this.gravityOfMoon = gravityOfMoon;
+
+            this.gravityOfEarth = gravityOfEarth;
+
+
+        }
+        public double GravitationOnTheMoon()
+        {
+            double calculeazaGravitate = weight * (gravityOfMoon / gravityOfEarth);
+
+            return calculeazaGravitate;
+        }
+
+
+    }
+
+}
---
+++ oop3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace oop3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = int.Parse(Console.ReadLine());
+
+            double gravityOfMoon = 1.62;
+
+            double gravityOfEarth = 9.807;
+
+
+            // facem instanta dupa un constructor 
+
+            Gravitation gravitation = new Gravitation(weight, gravityOfMoon, gravityOfEarth);
+
+            Console.WriteLine($"The weight {weight} on the moon is {gravitation.GravitationOnTheMoon()}");
+        }
+    }
+}
